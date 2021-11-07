7dc8e857c022a0570c33e9750fc7addcae67faba
Isabelle Ondracek <56898088+ikondra@users.noreply.github.com>
2021-11-06 23:43:40 -0400

    added some json handling

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, url_for
 from flask_socketio import SocketIO, emit, send
+
+import main
 import json
 
 # Flask documentation:
 # https://flask.palletsprojects.com/en/2.0.x/quickstart/#static-files
+app = Flask(__name__)
 
 # documentation to get Flask to support SocketIO:
 # https://flask-socketio.readthedocs.io/en/latest/getting_started.html
 # app.config['SECRET_KEY'] = 'secret!'
 
-app = Flask(__name__)
 socketio = SocketIO(app)
 
 
@@ -23,16 +25,12 @@ def scoreboard():
     return render_template("scoreboard.html")
 
 
-@app.route("/login")
-def login():
-    return "tbd"
-
-
 @socketio.on('answer')
-def button_pressed(json):
+def button_pressed(jsondata):
     # Handle the choice for the user
-    print(json)
-
+    data = json.loads(jsondata)
+    main.increment("connection thingy", data[data], -1)
+    print(jsondata)
 
     # Emit something for the user to say if they follow the hive or not!
     # emit('answer_callback', json.dumps({"You are with the hive!"}))
