a9eccc5fc55a3b4f672f795544fde5fbfca86f32
Isabelle Ondracek <56898088+ikondra@users.noreply.github.com>
2021-11-06 20:23:13 -0400

    socketio in progress

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
 
+# Flask documentation:
+# https://flask.palletsprojects.com/en/2.0.x/quickstart/#static-files
 app = Flask(__name__)
+
+# documentation to get Flask to support SocketIO:
+# https://flask-socketio.readthedocs.io/en/latest/getting_started.html
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route("/")
-def hello_world():
-    return ":)"
+def question_page():
+    return render_template("question.html")
+
+
+@app.route("/scoreboard")
+def scoreboard():
+    return render_template("scoreboard.html")
 
 
 @app.route("/login")
 def login():
-    return "login :)"
+    return "tbd"
+
+
+@socketio.on('my event')
+def button_pressed(json):
+    return "tbd"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
